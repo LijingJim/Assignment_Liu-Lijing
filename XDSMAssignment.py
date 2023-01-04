ac62245c949beafc903e12ea1ed186b35e8d6462
dahjeong <dah.jeong.kim@gmail.com>
2023-01-05 07:27:03 +0800

    change metamodel and ifunc, remove \vec{W}, create "try_assign" folder

--- XDSMAssignment.py
+++ XDSMAssignment.py
@@ -6,35 +6,29 @@ Description : XDSM Assignment
 
 from pyxdsm.XDSM import (
     XDSM,
-    OPT,
-    SUBOPT,
     SOLVER,
-    DOE,
     IFUNC,
     FUNC,
-    GROUP,
-    IGROUP,
     METAMODEL,
     LEFT,
-    RIGHT,
 )
 
 x = XDSM()
 
 # Create System
-x.add_system("MI", SOLVER, r"\text{Misson Integration}")
-x.add_system("MM", FUNC, r"\text{Mass Model}")
-x.add_system("GM", FUNC, r'\text{Gravitational Model}')
-x.add_system("CMCC", FUNC, r'\text{Constant Mach/CAS Conditions}')
-x.add_system("FD", SOLVER, r"\text{Flight Dynamics}")
+x.add_system("MI", SOLVER, (r"\text{Misson}", r"\text{Integration}"))
+x.add_system("MM", FUNC, (r"\text{Mass}", r"\text{Model}"))
+x.add_system("GM", FUNC, (r"\text{Gravitational}", r"\text{Model}"))
+x.add_system("CMCC", IFUNC, (r"\text{Constant Mach/CAS}", r"\text{Conditions}"))
+x.add_system("FD", SOLVER, (r"\text{Flight}", r"\text{Dynamics}"))
 x.add_system("IN", FUNC, "Integrator")
-x.add_system("AV", FUNC, r'\text{Angular Velocity}')
-x.add_system("BEF", FUNC, (r'\text{Body} \to \text{Earth}', r'\text{Frame}'))
+x.add_system("AV", FUNC, (r"\text{Angular}", r"\text{Velocity}"))
+x.add_system("BEF", FUNC, (r"\text{Body} \to \text{Earth}", r"\text{Frame}"))
 x.add_system("INT", FUNC, "Integrator")
-x.add_system("EOM", FUNC, r'\text{Equation of Motion}')
-x.add_system("AM", FUNC, r'\text{Atmospheric Model}')
-x.add_system("AEM", FUNC, r'\text{Aerodynamic Model}')
-x.add_system("PM", FUNC, r'\text{Propulsion Model}')
+x.add_system("EOM", FUNC, (r"\text{Equation}", r"\text{of Motion}"))
+x.add_system("AM", FUNC, (r"\text{Atmospheric}", r"\text{Model}"))
+x.add_system("AEM", METAMODEL, (r"\text{Aerodynamic}", r"\text{Model}")) 
+x.add_system("PM", METAMODEL, (r"\text{Propulsion}", r"\text{Model}"))
 
 # Create Connection
 x.connect("MI", "MM", "m", label_width=2)
@@ -71,7 +65,7 @@ x.connect("AM", "PM", [r'\rho',"p_{amb},T_{amb},M"],label_width=2)
 x.add_input("MI", ["m_0 , \Delta t"])
 x.add_output("MI", ["m_{fuel} , m_{ZFW}"], side=LEFT)
 x.add_input("CMCC", ["h^* , M^*, CAS^*"])
-x.add_input("FD", ["\Vec{r_0} , \Vec{W}V_0, \Vec{a_0}"])
+x.add_input("FD", ["\Vec{r_0} , \Vec{V_0}, \Vec{a_0}"])
 x.add_input("AEM", ["S_{wing}" , r'\alpha', r'\eta'], label_width=3)
 x.add_input("PM", ["BPR , n_{eng}, F_{T_{ref}}"])
 
@@ -81,4 +75,4 @@ x.add_process([ "PM","EOM", "INT", "BEF", "IN", "CMCC", "GM", "MM", "MI"], arrow
 
 # create pdf
 x.write("try_assign", cleanup=False)
-x.write_sys_specs("sink_specs")
+x.write_sys_specs("try_assign")
